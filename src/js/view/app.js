33855b31dca21f7cb1be6a651238c883c359f7e7
anandev <achmadsyaifulanand@gmail.com>
2024-08-06 13:27:21 +0700

    add element nav-bar

--- src/js/view/app.js
+++ src/js/view/app.js
@@ -1,5 +1,6 @@
 import "../components/index.js";
 import Notes from "../data/notes.js";
+import "../utils/modal.js";
 
 const app = () => {
   const cardList = document.querySelector(".card-list");
@@ -21,18 +22,36 @@ const app = () => {
     date.textContent = dateF.toDateString();
     noteItem.appendChild(date);
 
-    const bodyNote = document.createElement("p");
+    const bodyNote = document.createElement("div");
     bodyNote.setAttribute("slot", "body");
-    bodyNote.textContent = note.body;
+    const p = splitBodyNote(note);
+
+    bodyNote.appendChild(p);
     noteItem.appendChild(bodyNote);
+
+    noteItem.addEventListener("click", (e) => {
+      console.log("click", e.target);
+    });
 
     cardList.appendChild(noteItem);
   };
 
-  cardList.innerHTML = "";
-  const cardItems = notes.forEach((note) => createNoteItem(note));
+  const splitBodyNote = ({ body }) => {
+    const p = document.createElement("p");
+    body.split("\n").forEach((item) => {
+      p.innerHTML += `${item}<br>`;
+    });
+    return p;
+  };
 
-  console.log(cardItems);
+  const renderNotes = () => {
+    cardList.innerHTML = "";
+    notes.forEach((note) => createNoteItem(note));
+  };
+
+  setInterval(() => {
+    renderNotes();
+  }, 1000);
 };
 
 app();
